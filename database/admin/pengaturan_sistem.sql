39ae088424883ea44a8a872a6fe910e1fb138a4c
Cumikkk. <m.fahrulalfanani25@gmail.com>
2026-08-11 12:33:11 +0700

    feat: migrasi skema database v2, penataan folder database/admin, ekspor sql admin & toko_madura, serta update konteks-projek

--- database/admin/pengaturan_sistem.sql
+++ database/admin/pengaturan_sistem.sql
@@ -16,32 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `admin_module`
+-- Table structure for table `pengaturan_sistem`
 --
 
-DROP TABLE IF EXISTS `admin_module`;
+DROP TABLE IF EXISTS `pengaturan_sistem`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `admin_module` (
-  `id` int(11) NOT NULL,
-  `m_order` int(11) DEFAULT 1,
-  `group_id` int(11) NOT NULL,
-  `module` varchar(255) NOT NULL,
-  `status` int(11) DEFAULT -1,
-  `visible` int(11) DEFAULT -1,
-  `created_at` datetime DEFAULT current_timestamp(),
-  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+CREATE TABLE `pengaturan_sistem` (
+  `id_pengaturan` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_pengaturan` varchar(50) NOT NULL,
+  `nilai` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_pengaturan`),
+  UNIQUE KEY `nama_pengaturan` (`nama_pengaturan`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `admin_module`
+-- Dumping data for table `pengaturan_sistem`
 --
 
-LOCK TABLES `admin_module` WRITE;
-/*!40000 ALTER TABLE `admin_module` DISABLE KEYS */;
-INSERT INTO `admin_module` VALUES (1,1,1,'Dashboard',-1,-1,'2026-07-23 12:19:52',NULL),(2,2,2,'Investor',-1,-1,'2026-07-23 12:19:52',NULL),(4,1,3,'Admin',-1,-1,'2026-07-23 12:19:52',NULL),(5,1,4,'group',-1,-1,'2026-07-23 12:19:52',NULL),(6,2,4,'module',-1,-1,'2026-07-23 12:19:52',NULL),(7,1,5,'Outlet',-1,-1,'2026-07-23 15:47:15','2026-07-28 01:49:08'),(19,1,12,'data_master',-1,-1,'2026-07-28 15:00:51','2026-08-06 06:28:54'),(32,2,19,'rekening_bank',-1,-1,'2026-07-29 16:31:32',NULL),(33,2,12,'komisi',-1,-1,'2026-08-06 13:28:54',NULL);
-/*!40000 ALTER TABLE `admin_module` ENABLE KEYS */;
+LOCK TABLES `pengaturan_sistem` WRITE;
+/*!40000 ALTER TABLE `pengaturan_sistem` DISABLE KEYS */;
+INSERT INTO `pengaturan_sistem` VALUES (1,'potongan_global','10.00'),(2,'biaya_langganan_outlet','100000'),(3,'bank_nama','BRI'),(4,'bank_no_rekening','987-654-321'),(5,'bank_atas_nama','Anonymous');
+/*!40000 ALTER TABLE `pengaturan_sistem` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -53,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-11 12:32:57
+-- Dump completed on 2026-08-11 12:32:58
